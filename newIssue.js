5ae2eeb87fdeaf4e07b772e7351e13e7c985dd47
veselv2010 <veselv2010@gmail.com>
2020-06-06 21:23:41 +0300

    рендер issues из массива

--- newIssue.js
+++ newIssue.js
@@ -1,17 +1,28 @@
+var issues = [
+    'Disappearing beatmaps',
+];
+
 function newIssue(){
     let issueName = prompt("Issue name", "IssueName");
-    document.getElementById("mainBody").innerHTML += "<div class='issue'>" +
-    "<div class='flexRow'>"+
-        "<a href='issues.html' class='issueName'>"+issueName+"</a>"+
-    "</div>"+
-    "<div class='flexRow'>"+
-        "<span class='issueInfo'>#ofIssue opened by</span>"+
-        "<a href='issues.html' class='issueInfo'>userName</a>"+
-    "</div>"+
-"</div>";
+    issues.push(issueName);
 
-    let oldText = document.getElementById("openIssuesCounter").innerText;
-    let issuesCount = Number(oldText.split(" ")[0]);
-    issuesCount += 1;
-    document.getElementById("openIssuesCounter").innerText = issuesCount + " Open";
+    renderIssues();
+}
+
+function renderIssues(){
+    document.getElementById("mainBody").innerHTML = null;
+
+    for(elem in issues){
+        document.getElementById("mainBody").innerHTML += "<div class='issue'>" +
+        "<div class='flexRow'>"+
+            "<a href='issues.html' class='issueName'>"+issues[elem]+"</a>"+
+        "</div>"+
+        "<div class='flexRow'>"+
+            "<span class='issueInfo'>#ofIssue opened by</span>"+
+            "<a href='issues.html' class='issueInfo'>userName</a>"+
+        "</div>"+
+    "</div>";
+    }
+
+    document.getElementById("openIssuesCounter").innerText = issues.length + " Open";
 }
